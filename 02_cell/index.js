9b82b1f33fe7e7bbe9fb2064a9f0577d3818d12d
TZ <atian25@qq.com>
2018-10-26 08:47:07 +0800

    refactor: tpl to app/view

--- 02_cell/index.js
+++ 02_cell/index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   // 注意：此处为简化示例，一般需要缓存，且一定不能使用 Sync 同步方法
-  const html = fs.readFileSync(path.join(__dirname, 'public/index.html'));
+  const html = fs.readFileSync(path.join(__dirname, 'app/view/index.html'));
   return res.end(html);
 });
 
