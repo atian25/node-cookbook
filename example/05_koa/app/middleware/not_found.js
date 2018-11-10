3335ff3e59cb16d29cabf248592b2b0b2e195391
TZ <atian25@qq.com>
2018-11-10 09:15:34 +0800

    fix: correct koa 404

--- example/05_koa/app/middleware/not_found.js
+++ example/05_koa/app/middleware/not_found.js
@@ -4,11 +4,9 @@ module.exports = () => {
   return async (ctx, next) => {
     await next();
 
-    // Node 默认的 status 为 200，故这里判断 body 为空的时候，为没处理。
-    if (ctx.body === undefined && ctx.status === 200) {
+    if (ctx.status === 404) {
       const msg = `[Error] ${ctx.method} ${ctx.url} not found`;
       console.warn(msg);
-      ctx.status = 404;
       ctx.body = msg;
     }
   };
